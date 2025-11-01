4e0500527a0de4acde175e79e4efdda158e899c9
Alsechemist <alsechemist@gmail.com>
2025-11-01 19:55:54 +0600

    organized frame files into /source/main directory

--- source/conf.py
+++ source/conf.py
@@ -14,6 +14,8 @@ release = '1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
+master_doc = 'main/index'
+
 extensions = []
 
 templates_path = ['_templates']
